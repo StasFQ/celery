cbcf94ba19b69f5dac1b80a45644f95f74fc89e6
stas <stas37951@gmail.com>
2022-11-16 19:03:08 +0200

    Homework done

--- core/views.py
+++ core/views.py
@@ -13,6 +13,6 @@ def contact_form(request):
             time = form.cleaned_data['time']
             email = form.cleaned_data['email']
             message = form.cleaned_data['message']
-            tasks.send_mail.apply_async((time, email, message), etc=time)
+            tasks.send_mail.apply_async((time, email, message), eta=time)
             return redirect('core:contact_form')
     return render(request, 'contact.html', {'form': form})
